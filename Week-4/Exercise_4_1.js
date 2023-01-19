52dfe89053fade2e01884e61b2f6bc62a4423877
Arpan Maurya <scammers@Arpans-MacBook-Pro.local>
2023-01-19 21:51:30 +0530

    fix exercise 4.1 with custom promise

--- Week-4/Exercise_4_1.js
+++ Week-4/Exercise_4_1.js
@@ -1,15 +1,36 @@
+function customPromise(executor) {
+  let onResolve;
+  let onReject;
+  this.then = function (fn) {
+    onResolve = fn;
+    return this;
+  };
+  this.catch = function (fn) {
+    onReject = fn;
+    return this;
+  };
+  function resolve(val) {
+    onResolve(val);
+  }
+  function reject(val) {
+    onReject(val);
+  }
+  executor(resolve, reject);
+}
+
 const getNumber = () =>
-  new Promise((res, rej) => {
+  new customPromise((res, rej) => {
     const randomNumber = parseInt(Math.random() * 100, 10);
     setTimeout(() => {
       if (randomNumber % 5 === 0) {
-        rej(`Rejected with num: ${randomNumber}`);
+        return rej(`Rejected with num: ${randomNumber}`);
       }
       res(`Resolved with num: ${randomNumber}`);
     }, randomNumber * 10);
   });
 
 const numberPromise = getNumber();
+
 numberPromise
   .then((number) => {
     console.log(number);
